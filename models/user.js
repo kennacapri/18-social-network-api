5e192b4eaab7589ca7869a581d99e4bde37844b4
Kenna Fackrell <kennac28@gmail.com>
2023-01-30 16:23:00 -0700

    created a model for the user, email, thoughts and friends

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: "string",
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: "string",
+      unique: true,
+      required: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
