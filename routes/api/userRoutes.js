cc1fafded383445d8f8a5ce431f02c3e48d64526
Kenna Fackrell <kennac28@gmail.com>
2023-02-02 15:46:18 -0700

    fixed typo error

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
-    getAllUsers,
-    getSingleUser,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    deleteFriend
-} = require('../../controllers/userController');
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
 
 // routes for getting users, single user,create update, delete, add friend and delete friend
-router.route('/').get(getAllUsers).post(createUser);
+router.route("/").get(getAllUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
